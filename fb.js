77189372401b350230c8335160afc80cb49e8e2f
Apple-PC\Apple <akashp.inno@outlook.com>
2018-04-13 15:41:47 +0530

    fb callback url

--- fb.js
+++ fb.js
@@ -2,10 +2,11 @@
 module.exports = {
 	'appID' : '161650311174180',
 	'appSecret' : '257288690f8d8750a080cc065bb967f6',
-	'callbackUrl' : 'https://daywhisper.auth0.com/login/facebook/callback'
+	'callbackUrl' : 'https://daywhispersocial.herokuapp.com/login/facebook/callback'
 }	
 
 
 //https://localhost:3000/login/facebook/callback
 //https://daywhisper.auth0.com/login/facebook/callback
 //https://localhost:3000/auth/login/facebook/callback
+//https://daywhispersocial.herokuapp.com/login/facebook/callback
