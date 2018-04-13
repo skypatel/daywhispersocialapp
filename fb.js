e092ad4544da57238d7e37495beba695d67c225e
Apple-PC\Apple <akashp.inno@outlook.com>
2018-04-13 14:51:53 +0530

    facebook anf twitter login with passbook

---
+++ fb.js
@@ -0,0 +1,11 @@
+// facebook app settings
+module.exports = {
+	'appID' : '161650311174180',
+	'appSecret' : '257288690f8d8750a080cc065bb967f6',
+	'callbackUrl' : 'https://daywhisper.auth0.com/login/facebook/callback'
+}	
+
+
+//https://localhost:3000/login/facebook/callback
+//https://daywhisper.auth0.com/login/facebook/callback
+//https://localhost:3000/auth/login/facebook/callback
